ec4980b15a529958477afa20ea368e7e424bd221
aoudiasofia <sofia.aoudia@icloud.com>
2025-04-09 22:07:58 +0200

    V

--- fichier.h
+++ fichier.h
@@ -6,24 +6,13 @@
 #include <time.h>
 #include <string.h>
 
-//structure pour les personnages 
-typedef struct{
-    int points_de_vie_courants; //verifier si négatifs ou nuls, si KO ne peut plus participer jusqu'à la fin du combat 
-    int points_de_vie_max;  //limite en cas de soins
-    int attaque; //dégats de base infliger
-    int defense; //calcul pourcentage de réduction de dégats 
-    int vitesse; // pourcentage chances pour éviter un attaque
-    TechniqueSpeciale competencesSpeciales;  // plusieurs actions à utiliser au lieu d’attaquer, et qui permettront, soit d’affecter un état spécial bénéfique à un allié, ou au contraire affaiblir un des
-combattants adverses  
-} Combattants;
-
 
 //énumération des opérations
 typedef enum{
-      AJOUT,
-    SOUSTRACTION,
-    MULTIPLICATION,
-    DIVISION
+    addition,
+    soustraction,
+    multiplication,
+    divison
 } Operation;
 
 
@@ -36,3 +25,16 @@ typedef struct {
     int nbToursRecharge;              // temps de recharge ( en nombre de tours)
     Operation operation;              // type d'opération 
 } TechniqueSpeciale;
+
+
+//structure pour les personnages 
+typedef struct{
+    int points_de_vie_courants; //verifier si négatifs ou nuls, si KO ne peut plus participer jusqu'à la fin du combat 
+    int points_de_vie_max;  //limite en cas de soins
+    int attaque; //dégats de base infliger
+    int defense; //calcul pourcentage de réduction de dégats 
+    int vitesse; // pourcentage chances pour éviter un attaque
+    TechniqueSpeciale competencesSpeciales; 
+} Combattants;
+
+
